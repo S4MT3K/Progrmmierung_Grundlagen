d3ffb78641cc5ad6fa28f78af22a556010755eb3
s4mt3k <3rdlvl@gmx.de>
2025-10-01 15:04:42 +0200

    Aufgabe

---
+++ 03_strings.php
@@ -0,0 +1,45 @@
+<?php
+# Ein String ist eine Sequenz von Zeichen
+# strings werden mit ' oder " geschrieben.
+
+$my_string = "Hallo Welt!";
+$my_string2 = 'Hallo 2. Welt';
+
+# in " " ist es möglich variablen und spezielle zeichen zu verwenden.
+// (escaping) LEIDER NUR MIT <pre> möglich... PHP is doof
+echo "<pre>Hallo Welt! \t das war ein Tabstop </pre>";
+
+$name = "Säm";
+echo "Hallo $name!"; //gibt den Wert der variable in dem Funktionalen string aus. also " ".
+// 'Hallo $name!' gibt Hallo $name! als string direkt aus ohne die variable zu nehmen.
+
+
+# spezielle Zeichen leitet man mit einem \ ein
+
+#<br> -> newline
+echo "hallo<br>$name";
+echo "<br>";
+# \t tabulator
+echo "hallo\t$name";
+echo "<br>";
+# \\ ume ein \ zu machen
+echo "c:\\user\\$name";
+echo "<br>";
+# \" um eine " in einem string dazustellen
+echo "der \"$name\"";
+echo "<br>";
+
+echo strlen($name); // zählt die buchstaben bzw. bits eines Wortes
+echo "<br>";          // umlaute und spezielle zeichen werden zusätzlich gezählt
+
+$wort = "Hallo Welt, dies ist ein Test";
+
+echo str_word_count($wort); // Direkter weg zum ausgeben des returnwerts der Build-In Function str_word_count
+echo "<br>";
+
+// man kann strings miteinander verbinden (String - Concatination)
+
+$vornamen = "Max";
+$nachnamen = "Mustermann";
+
+//Aufgabe: Schreibe eine Funktion, die vor- und nachname miteinander verbindet und ein echo ausgibt mit "Hallo Max Mustermann".
