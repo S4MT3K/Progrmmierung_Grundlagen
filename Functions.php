3f7186002c2e7414586511771f0319eb95da4115
s4mt3k <3rdlvl@gmx.de>
2025-10-01 09:48:24 +0200

    Best Version ever

--- Functions.php
+++ Functions.php
@@ -1,2 +1,19 @@
 <?php
 //Hier kommen Aufgaben über das Thema Programmierung
+//Insbesondere Funktionen
+
+//Aufgabe lasst euch per funktion die unten stehenden echos ausgeben:
+
+function helloWorldEchoPrintForTheWin()
+{
+    echo "<pre>";
+    echo "Hello World\n";
+    echo "Hello World My Name is Sam\n";
+    echo "Hello World I Like Programming\n";
+    echo "Hello World This BBQ-School is awesome\n";
+    echo " ''''''''''''' ";
+    echo "</pre>";
+}
+
+helloWorldEchoPrintForTheWin();
+
