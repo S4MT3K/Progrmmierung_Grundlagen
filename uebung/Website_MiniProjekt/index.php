be743b57801ad216828f9f72658412b0f37a78e5
s4mt3k <3rdlvl@gmx.de>
2025-10-15 15:34:53 +0200

    Website Project push

---
+++ uebung/Website_MiniProjekt/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Mini Website</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body style="background-color: gray">
+<div id="content_div" class="content_div"><!-- Content Div -->
+    <div id="navBar" class="navBar">
+        <div class="navElement">Home</div>
+        <div class="navElement">Pictures</div>
+        <div class="navElement">Impressum</div>
+        <div class="navElement">Login</div>
+    </div>
+    <div id="mainContent" class="mainContent">
+        <div class="mainContentElement">
+            <h1>Welcome to my Mini Website</h1>
+            <p>This is my first website. I hope you like it!</p>
+        </div>
+        <div style="width:50%; border: 1px solid black; margin: 0 auto;">
+            Hallo Ihr Amazing Peoples
+        </div>
+        <div style="width:50%; border: 1px solid black; margin: 0 auto;">
+            2.er Text
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
